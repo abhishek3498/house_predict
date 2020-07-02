943031b6037642fa91d274bacb116d63a13a86e8
abhishek3498 <67725095+abhishek3498@users.noreply.github.com>
2020-07-02 10:56:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,4 +20,5 @@ def predict():
     return render_template('index.html',prediction_text="Price of house Should be {}".format(math.floor(output)))
 
 
-app.run(host='0.0.0.0', port=8080)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
